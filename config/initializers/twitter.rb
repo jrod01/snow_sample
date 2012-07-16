c9a86066a26fd59157783a8f6c25a327fae861dd
Jonathan Rodriguez <jrguezg1@gmail.com>
2012-07-16 12:14:50 -0400

    snow sample

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,7 @@
 
 Twitter.configure do |config|
-  config.consumer_key = "bBYChWkdXqqhwMe23gimA"
-  config.consumer_secret = "Fk6kYGwtBESxjJ4Tji7EVdY3CdENC8CxxQ6mTSKEWr0"
-  config.oauth_token = "17154967-iwQSYGWHr02eiEGYNfXn6EepF2VtfPY8kLkYahqsA"
-  config.oauth_token_secret = "c24h7eeIEiqSAGXegkCE3Ueibaid95NGAehkJc22A"
+  config.consumer_key = " "
+  config.consumer_secret = " "
+  config.oauth_token = " "
+  config.oauth_token_secret = " "
 end
